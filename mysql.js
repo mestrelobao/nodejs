3e6080b372b23838eb3963c28aca5c74d7e2e38a
mestrelobao <mestre_lobao@yahoo.com>
2024-10-29 18:52:12 -0300

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -1,12 +1,14 @@
 var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456"
+var connection = mysql.createConnection({
+    host     : 'localhost',
+      user     : 'root',
+     password : '123456',
+      database : 'guiga'
 });
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
 });
