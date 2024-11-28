09bf654cf96245fb5a79e995046cecf65a0ea6b7
mestrelobao <mestre_lobao@yahoo.com>
2024-11-28 18:22:15 -0300

    Update https.js

--- https.js
+++ https.js
@@ -14,23 +14,10 @@ const cred = {
 key,
         cert
 }
-
 app.get('/', (req, res) => {
         res.send({
                 nome: 'guiga'
         });
 });
-//app.get('/.well-known/pki-validation/ECBA50B842CED36FB16B806135A0CA09.txt', (req, res) => {
-//res.sendFile('/home/ubuntu/ECBA50B842CED36FB16B806135A0CA09.txt');
-//});
 const httpsServer = https.createServer(cred, app);
 httpsServer.listen(8081);
-//app.listen(3000);
-//http.createServer(function (req, res) {
-//  fs.readFile('pagina.html', function(err, data) {
-//    res.writeHead(200, {'Content-Type': 'text/html'});
-//    res.write(data);
-//console.log('guiga');
-//        return res.end();
-//  });
-//}).listen(8080);
