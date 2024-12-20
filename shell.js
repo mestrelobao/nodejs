0f44cca189678421285302c94f0c1ca9ea851c04
mestrelobao <mestre_lobao@yahoo.com>
2024-12-20 01:06:27 -0300

    Delete shell.js

--- shell.js
+++
@@ -1,8 +0,0 @@
-const exec = require ('child_process').exec;
-exec('pm2 list', (e, stdout, stderr) => {
-if (e instanceof Error) {
-        console.error(e);
-        throw e;
-}
-console.log(stdout);
-});
